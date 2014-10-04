84409396a885493d98ff4e700be37bb9c977e801
Ask Holme <ask@askholme.dk>
2014-10-04 16:57:59 +0200

    various

--- dobuild.sh
+++ dobuild.sh
@@ -1,4 +1,4 @@
-TMP_NAME="askholme/baseimage"
+TMP_NAME="askholme/baseimage-tmp"
 NAME="askholme/baseimage:squashed"
 docker build -t $TMP_NAME .
 docker save $TMP_NAME | docker-squash -from root -t $NAME -verbose | docker load
